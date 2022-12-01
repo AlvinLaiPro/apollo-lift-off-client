82ca354a201f48ad896674d2e27aa31f7b304b04
Patrick Lai <patrick.lai@ringcentral.com>
2022-12-01 11:17:27 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyles from './styles';
+import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://apollo-lift-off-production.up.railway.app/',
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <GlobalStyles />
+      <Pages />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
